d6fc7b66daa298b645d2a25a93b2c4294b54c599
morawararuna <146064070+morawararuna@users.noreply.github.com>
2023-11-03 12:03:52 +0530

    Add files via upload

---
+++ DegitsRepetation.java
@@ -0,0 +1,28 @@
+
+public class DegitsRepetation {
+
+	public static void main(String[] args) {
+		String str = "105577979356747";
+		// 10339093414
+		//
+		// last repeated number
+		char[] strArr = str.toCharArray();
+		char eachChar;
+		int isCount = 0;
+		char lastRepChar = ' ';
+		for (int i = 0; i < strArr.length; i++) {
+			eachChar = strArr[i];
+			isCount = 0;
+			for (int j = 0; j < strArr.length; j++) {
+				if (eachChar == strArr[j]) {
+					isCount ++;
+				} // if
+			} // for j
+			if (isCount > 1) {
+				lastRepChar = eachChar;
+			} // if
+		} // for i
+
+		System.out.println("Last Repeated Char--->" + lastRepChar);
+	}
+}
